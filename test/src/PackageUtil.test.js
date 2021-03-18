ec92112fb1586f146497eb91b665703da441136d
typhonrt <support@typhonrt.org>
2021-03-18 05:54:34 -0700

    0.0.1 release

--- test/src/PackageUtil.test.js
+++ test/src/PackageUtil.test.js
@@ -6,7 +6,7 @@ import PackageUtil   from '../../src/index.js';
 
 const packageObj = JSON.parse(fs.readFileSync(`./package.json`, 'utf8'));
 
-const s_VERIFY_DATA = '{"name":"@typhonjs-node-utils/package-util","type":"module","description":"Provides several utility methods for working with `package.json`.","author":"typhonrt","homepage":"https://github.com/typhonjs-node-utils/package-util","license":"MPL-2.0","main":"src/index.js","repository":{"type":"github","url":"https://github.com/typhonjs-node-utils/package-util"},"bugs":{"url":"https://github.com/typhonjs-node-utils/package-util/issues"},"formattedMessage":"name: @typhonjs-node-utils/package-util (0.0.0)\\ndescription: Provides several utility methods for working with `package.json`.\\nbugs / issues: https://github.com/typhonjs-node-utils/package-util/issues\\nrepository: https://github.com/typhonjs-node-utils/package-util\\nhomepage: https://github.com/typhonjs-node-utils/package-util"}';
+const s_VERIFY_DATA = (version) => `{"name":"@typhonjs-node-utils/package-util","version":"${version}","type":"module","description":"Provides several utility methods for working with \`package.json\`.","author":"typhonrt","homepage":"https://github.com/typhonjs-node-utils/package-util","license":"MPL-2.0","main":"src/index.js","repository":{"type":"github","url":"https://github.com/typhonjs-node-utils/package-util"},"bugs":{"url":"https://github.com/typhonjs-node-utils/package-util/issues"},"formattedMessage":"name: @typhonjs-node-utils/package-util (${version})\\ndescription: Provides several utility methods for working with \`package.json\`.\\nbugs / issues: https://github.com/typhonjs-node-utils/package-util/issues\\nrepository: https://github.com/typhonjs-node-utils/package-util\\nhomepage: https://github.com/typhonjs-node-utils/package-util"}`;
 
 describe('PackageUtil:', () =>
 {
@@ -14,8 +14,6 @@ describe('PackageUtil:', () =>
    {
       const data = PackageUtil.format(packageObj);
 
-      delete data.version;
-
-      assert.strictEqual(JSON.stringify(data), s_VERIFY_DATA);
+      assert.strictEqual(JSON.stringify(data), s_VERIFY_DATA(data.version));
    });
 });
