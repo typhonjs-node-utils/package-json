93ff9d0d5523ef690096794f61f979420ab64dd1
typhonrt <support@typhonrt.org>
2021-06-04 05:24:32 -0700

    update for eventbus options

--- src/plugin.js
+++ src/plugin.js
@@ -9,9 +9,19 @@ export function onPluginLoad(ev)
 {
    const eventbus = ev.eventbus;
 
-   eventbus.on('typhonjs:utils:package:json:format', PU.formatPackage, void 0, true);
-   eventbus.on('typhonjs:utils:package:json:format:get', PU.getPackageAndFormat, void 0, true);
-   eventbus.on('typhonjs:utils:package:json:get', PU.getPackage, void 0, true);
-   eventbus.on('typhonjs:utils:package:json:path:get', PU.getPackageWithPath, void 0, true);
-   eventbus.on('typhonjs:utils:package:json:type:get', PU.getPackageType, void 0, true);
+   const options = ev.pluginOptions;
+
+   let guard = true;
+
+   // Apply any plugin options.
+   if (typeof options === 'object')
+   {
+      if (typeof options.guard === 'boolean') { guard = options.guard; }
+   }
+
+   eventbus.on('typhonjs:utils:package:json:format', PU.formatPackage, void 0, { guard });
+   eventbus.on('typhonjs:utils:package:json:format:get', PU.getPackageAndFormat, void 0, { guard });
+   eventbus.on('typhonjs:utils:package:json:get', PU.getPackage, void 0, { guard });
+   eventbus.on('typhonjs:utils:package:json:path:get', PU.getPackageWithPath, void 0, { guard });
+   eventbus.on('typhonjs:utils:package:json:type:get', PU.getPackageType, void 0, { guard });
 }
