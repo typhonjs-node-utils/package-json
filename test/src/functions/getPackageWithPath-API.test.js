8bc576dc8ca912ee41fd42ab1874ee9bde38b867
typhonrt <support@typhonjs.io>
2024-01-06 22:19:04 -0800

    Temporary test for Node version error string differences.

--- test/src/functions/getPackageWithPath-API.test.js
+++ test/src/functions/getPackageWithPath-API.test.js
@@ -115,6 +115,8 @@ if (test.getPackageWithPath_API)
          assert.isTrue(fs.existsSync(filepath));
          assert.isTrue(fs.existsSync(filepathUnix));
 
+         console.log(`!!! ERROR STRING: `, error.toString())
+
          // On Windows the character position for the error is different so just check the start of the error message.
          assert.isTrue(error.toString().startsWith(`SyntaxError: Expected property name or '}' in JSON at position`));
       });
